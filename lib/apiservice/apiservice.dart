53b4b74b153ca6d899930f4623bf13497574bedd
Syed Tanjim Ahmed <tanjimahmed327@gmail.com>
2023-12-15 00:17:35 +0600

    1strealese

---
+++ lib/apiservice/apiservice.dart
@@ -0,0 +1,10 @@
+import 'package:newsapp/modal/newsmodal.dart';
+
+class ApiService {
+  final String url =
+      "https://newsapi.org/v2/everything?q=*&from=2023-11-29&to=2023-11-29&sortBy=popularity&apiKey=";
+  final String apikey = "6e9bbb1e4bb54798a0d61bcc8ee95dd1";
+  Future<<List<NewsModel>>getnews()async{
+    Response 
+  }
+}
